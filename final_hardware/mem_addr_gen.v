143308bd0d31178483c6f611df3671fb2a84b699
tingenchen <ss113062309@gapp.nthu.edu.tw>
2025-12-21 03:53:52 +0800

    第二隻加入!!
    右邊那隻，用鍵盤移動，因為不會碰到GATE所以邏輯少寫一些，但地圖還沒用好所以現在看起來會有一點點怪。反正跟角色有關後面有_1的就是他的程式，基本上和原本那隻一模一樣

--- final_hardware/mem_addr_gen.v
+++ final_hardware/mem_addr_gen.v
@@ -5,10 +5,15 @@ module mem_addr_gen(
     input [9:0] v_cnt,      // VGA 當前掃描的垂直像素座標 (0-479)
     input vsync,            // 垂直同步訊號，用來在每幀畫面的開頭同步數據
     input [9:0] img_x,      // 來自控制邏輯的角色 X 座標
+    input [9:0] img_x_1,
     input [9:0] img_y,      // 來自控制邏輯的角色 Y 座標
+    input [9:0] img_y_1,
     input [2:0] frame_idx,  // 當前動畫播放到第幾幀 (例如 0~3 或 0~5)
+    input [2:0] frame_idx_1,
     input is_moving,        // 角色是否正在移動 (決定播放哪組動畫)
+    input is_moving_1,
     input face_left,        // 角色是否面向左邊 (決定是否鏡像翻轉)
+    input face_left_1,
     input [4:0] gate_open,      // 新增：門是否開啟 (來自 Top)
     output reg [16:0] pixel_addr,     // 輸出給 BRAM 的讀取地址 (1D)
     output wire out_show_pixel,        // 輸出給 Top 的顯示開關 (經過延遲同步)
@@ -24,14 +29,22 @@ module mem_addr_gen(
     // 目的：為了解決搖桿不同步產生的雜訊線。
     // 在每幀畫面的 vsync 期間才鎖定一次 img_x/y，確保在繪製同一幀畫面時，角色的座標是固定的。
     reg [9:0] x_s, y_s;
+    reg [9:0] x_s_1, y_s_1;
     always @(posedge vsync or posedge rst) begin
-        if (rst) {x_s, y_s} <= {10'd64, 10'd416};
-        else {x_s, y_s} <= {img_x, img_y};
+        if (rst) begin 
+            {x_s, y_s} <= {10'd32, 10'd416}; 
+            {x_s_1, y_s_1} <= {10'd608, 10'd416}; 
+        end
+        else begin
+            {x_s, y_s} <= {img_x, img_y};
+            {x_s_1, y_s_1} <= {img_x_1, img_y_1};
+        end
     end
 
     // --- 2. 區域判斷 (Combinational Logic - T0 階段) ---
     // 判斷當前 VGA 掃描點 (h_cnt, v_cnt) 是否落在角色區域內
     wire is_char = (h_cnt >= x_s + 3 && h_cnt < x_s + IMG_W - 3) && (v_cnt >= y_s + 5 && v_cnt < y_s + IMG_H);
+    wire is_char_1 = (h_cnt >= x_s_1 + 3 && h_cnt < x_s_1 + IMG_W - 3) && (v_cnt >= y_s_1 + 5 && v_cnt < y_s_1 + IMG_H);
     
     // 將螢幕座標轉為網格座標 (>> 5 等於除以 32)，用來查找 20x15 的地圖陣列
     wire [4:0] gx = h_cnt >> 5; 
@@ -62,7 +75,7 @@ module mem_addr_gen(
     assign map[8]  = {20{T_EMPTY}};
     assign map[9]  = {{10{T_WALL}}, {10{T_EMPTY}}}; 
     assign map[10] = {20{T_EMPTY}};
-    assign map[11] = {{20{T_PLATE_1}}, {5{T_EXIT}}, {3{T_PLATE_1}}, {2{T_GATE_1}}};
+    assign map[11] = {{10{T_PLATE_1}}, {5{T_EXIT}}, {3{T_PLATE_1}}, {2{T_GATE_1}}};
     assign map[12] = {20{T_EMPTY}};
     assign map[13] = {{7{T_EMPTY}}, T_GATE_1, {4{T_EMPTY}}, T_GATE_2, {4{T_EMPTY}}, T_GATE_3, {2{T_EMPTY}}};
     assign map[14] = {{5{T_WALL}}, {5{T_PLATE_1}}, {5{T_PLATE_2}}, {5{T_PLATE_3}}};
@@ -80,13 +93,14 @@ module mem_addr_gen(
                    (current_tile_id == T_GATE_3 && !gate_open[2]);
     
     // 最終顯示開關：只要是角色或是地圖塊就要亮起顏色
-    wire comb_show = is_char || is_tile;
+    wire comb_show = is_char || is_char_1 || is_tile;
 
     // --- 4. 地址計算 (組合邏輯) ---
     reg [7:0] coeff;     // 當前讀取圖片的總寬度 (換行計算用)
     reg [9:0] lx, ly;    // 在單一圖塊內的局部 X, Y 座標
     reg [16:0] b_off;    // 在 COE 檔案中的起始偏移地址
     wire [4:0] rel_x = h_cnt - x_s; // 掃描點相對於角色左上角的 X 距離 (0-31)
+    wire [4:0] rel_x_1 = h_cnt - x_s_1;
 
     always @(*) begin
         // 預設初始化，防止產生 Latch
@@ -127,6 +141,23 @@ module mem_addr_gen(
                 b_off = 1024;
                 coeff = 128;
             end
+        end else if (is_char_1) begin 
+            // 角色模式
+            ly = (v_cnt - y_s_1); // 垂直相對座標
+            
+            // 處理鏡像與動畫位址
+            if (is_moving_1) begin
+                // 走路動畫 (Walk): 寬度 192, 位於位址 8192
+                // (face_left ? (31 - rel_x) : rel_x) 實現硬體鏡像翻轉
+                lx = (face_left_1 ? (5'd31 - rel_x_1) : rel_x_1) + (frame_idx_1 * 32);
+                b_off = 5120;
+                coeff = 192;
+            end else begin
+                // 待機動畫 (Idle): 寬度 128, 位於位址 4096
+                lx = (face_left_1 ? (5'd31 - rel_x_1) : rel_x_1) + (frame_idx_1 * 32);
+                b_off = 1024;
+                coeff = 128;
+            end
         end 
     end
 
@@ -152,7 +183,7 @@ module mem_addr_gen(
             
             // 將顯示開關推入移位暫存器
             //delay_pipe <= {delay_pipe[2:0], comb_show};
-            delay_pipe <= {delay_pipe[2:0], (is_char || (current_tile_id != 4'h0))};
+            delay_pipe <= {delay_pipe[2:0], (is_char || is_char_1 || (current_tile_id != 4'h0))};
             // Tile ID 也要同步延遲 3 拍 (對齊 delay_pipe[2])
             id_pipe_1 <= current_tile_id;
             id_pipe_2 <= id_pipe_1;
